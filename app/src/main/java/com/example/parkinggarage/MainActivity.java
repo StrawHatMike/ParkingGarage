6b8aaf15342cb3f2d0598ca00ec227e203e330fb
StrawHatMike <klahm64@mail.sunysuffolk.edu>
2019-04-15 22:37:04 -0400

    Firebase Work

--- app/src/main/java/com/example/parkinggarage/MainActivity.java
+++ app/src/main/java/com/example/parkinggarage/MainActivity.java
@@ -1,15 +1,47 @@
 package com.example.parkinggarage;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.firebase.client.Firebase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Firebase mRootRef;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Firebase.setAndroidContext(this);
+        mRootRef = new Firebase("https://parkinggarage-d8ff3.firebaseio.com");
+        mAuth = FirebaseAuth.getInstance();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    public void onClick1(View view) {
+        //Intent intent = new Intent(this, RegisterActivity.class);
+        //startActivity(intent);
+        String email = "a@a.com";
+        String password = "a12345";
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, "Error Signing In", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Signed in!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
 
 }
